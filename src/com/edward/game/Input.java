18dddf0955c5723e708141db78a7edbd9daf501f
EdwardHoward <edwardhoward11@gmail.com>
2021-08-17 23:16:42 -0600

    Make KeyBindings statics final

--- src/com/edward/game/Input.java
+++ src/com/edward/game/Input.java
@@ -3,9 +3,9 @@ package com.edward.game;
 import java.awt.event.KeyEvent;
 
 public class Input {
-	public static int LEFT = 0;
-	public static int RIGHT = 1;
-	public static int JUMP = 2;
+	public static final int LEFT = 0;
+	public static final int RIGHT = 1;
+	public static final int JUMP = 2;
 	
 	public boolean[] buttons = new boolean[3];
 	public boolean[] oldButtons = new boolean[3];
